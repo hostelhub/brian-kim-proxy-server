7665de96c26f8d71a84f5954b336aa59f626a5e6
Brian Kim <brianjunewookim@gmail.com>
2019-04-10 14:55:45 -0700

    Implement proxy server

--- server/index.js
+++ server/index.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const path = require('path');
+const axios = require('axios');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, '../public')));
+app.use('/hostels/:hostelId', express.static(path.join(__dirname, '../public')));
+
+app.get('/api/hostels/:hostelId', (req, res) => {
+  const Id = req.params.hostelId;
+  axios.get(`http://localhost:3001/api/hostels/${Id}`)
+    .then(hostels => (res.status(200).send(hostels.data)))
+    .catch(err => console.log(err))
+});
 
 app.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`);
