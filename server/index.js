9acabaf348b8efd2c3b4de76e089472ffd3bdf33
Brian Kim <brianjunewookim@gmail.com>
2019-04-11 14:11:13 -0700

    Add get request for photos component

--- server/index.js
+++ server/index.js
@@ -6,12 +6,20 @@ const port = process.env.PORT || 3000;
 
 app.use('/hostels/:hostelId', express.static(path.join(__dirname, '../public')));
 
+app.get('/api/hostels/:id', (req, res) => {
+  const Id = req.params.id;
+  axios.get(`http://localhost:3001/api/hostels/${Id}`)
+    .then(response => (res.status(200).send(response.data)))
+    .catch(err => console.log(err))
+})
+
 app.get('/api/hostels/:hostelId', (req, res) => {
   const Id = req.params.hostelId;
   axios.get(`http://localhost:3002/api/hostels/${Id}`)
     .then(hostels => (res.status(200).send(hostels.data)))
     .catch(err => console.log(err))
 });
+
 
 app.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`);
