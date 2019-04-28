dfbd30b6a9064ad396c15ce4649ca94c5d2bfbc7
Brian Kim <brianjunewookim@gmail.com>
2019-04-27 17:19:59 -0700

    Revert due to accidental push

--- server/index.js
+++ server/index.js
@@ -2,25 +2,31 @@ const express = require('express');
 const path = require('path');
 const axios = require('axios');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 9000;
 
 app.use('/hostels/:hostelId', express.static(path.join(__dirname, '../public')));
 
-app.get('/api/hostels/:id', (req, res) => {
-  const Id = req.params.id;
-  axios.get(`http://localhost:3001/api/hostels/${Id}`)
-    .then(response => (res.status(200).send(response.data)))
+app.get('/api/hostels/:id/photos', (req, res) => {
+  const id = req.params.id;
+  axios.get(`http://34.205.69.191/api/hostels/${id}/photos`)
+    .then(hostels => (res.status(200).send(hostels.data)))
     .catch(err => console.log(err))
 })
 
-app.get('/api/hostels/:hostelId', (req, res) => {
-  const Id = req.params.hostelId;
-  axios.get(`http://localhost:3002/api/hostels/${Id}`)
+app.get('/api/hostels/:hostelId/calendar', (req, res) => {
+  const id = req.params.hostelId;
+  axios.get(`http://18.218.213.148/api/hostels/${id}/calendar`)
     .then(hostels => (res.status(200).send(hostels.data)))
     .catch(err => console.log(err))
 });
 
+app.get('/api/reviews/:hostelId/reviews', (req, res) => {
+  const id = req.params.hostelId;
+  axios.get(`http://localhost:3003/api/reviews/${id}/reviews`)
+    .then(hostels => (res.status(200).send(hostels.data)))
+    .catch(err => console.log(err))
+});
 
 app.listen(port, () => {
-  console.log(`server running at: http://localhost:${port}`);
+  console.log(`server running at: http://18.222.164.21:${port}`);
 });
